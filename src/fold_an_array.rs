0463079fd24c1ff793af17bfa507587b319d30a3
KyleHuang <upday7@163.com>
2021-10-17 13:59:16 +0800

    style: commented fold_an_array.rs;

--- src/fold_an_array.rs
+++ src/fold_an_array.rs
@@ -7,9 +7,15 @@ fn fold_array(arr: &[i32], runs: usize) -> Vec<i32> {
     let mut v = arr.to_vec();
     for _ in 0..runs {
         let n = v.len();
-        let m = if n & 1 == 1 { n / 2 } else { 1 + n / 2 };
+        let m = if n & 1 == 1 {
+            // is odd
+            n / 2
+        } else {
+            // is even
+            1 + n / 2
+        };
         for i in 0..m {
-            v[i] += v[n - 1 - i];
+            v[i] += v[n - 1 - i]; // add up last m to top m elements
         }
         v.resize(if n & 1 == 1 { 1 + n / 2 } else { n / 2 }, 0);
     }
@@ -19,6 +25,15 @@ fn fold_array(arr: &[i32], runs: usize) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn bitwiseandtest() {
+        assert_eq!(0 & 1, 0);
+        assert_eq!(1 & 1, 1);
+        assert_eq!(2 & 1, 0);
+        assert_eq!(3 & 1, 1);
+        assert_eq!(4 & 1, 0);
+    }
 
     #[test]
     fn basic() {
